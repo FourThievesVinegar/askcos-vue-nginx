635886a34c4cee994eed9618dee6f5d51bd07059
Samuel Gong <gosamuel719@gmail.com>
2023-08-11 16:00:08 -0400

    added buyable route

--- askcos_vue/src/router/index.js
+++ askcos_vue/src/router/index.js
@@ -44,6 +44,20 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/buyables',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Buyables',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/buyables/App.vue'),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
