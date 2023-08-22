68840c5b40d83b5b8411e85ba88fa2611b59c207
sjchoure <sourabhchoure98@gmail.com>
2023-08-22 03:04:44 -0400

    More features to IPP and some fix

--- askcos_vue/src/layouts/default/AppBar.vue
+++ askcos_vue/src/layouts/default/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer expand-on-hover rail elevation="4" @update:rail="onDrawerCollapse" width="100px">
+  <v-navigation-drawer expand-on-hover rail elevation="2" @update:rail="onDrawerCollapse" width="100px">
     <v-list>
       <v-list-item :prepend-avatar="logoSrc" title="ASKCOS" subtitle="Demo"></v-list-item>
     </v-list>
@@ -10,7 +10,8 @@
       <v-list-item prepend-icon="mdi-home" title="Home" to="/" value="home"></v-list-item>
       <v-list-group value="modules" no-action>
         <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-file-tree" title="Modules"></v-list-item>
+          <v-list-item v-bind="props" prepend-icon="mdi-file-tree" title="Modules" :active="activeModules"
+            :variant="activeModules ? 'tonal' : 'text'"></v-list-item>
         </template>
         <v-list-item prepend-icon="mdi-help-box" title="Overview" value="overview"></v-list-item>
         <v-list-group value="Retrosynthesis" subgroup>
@@ -49,7 +50,8 @@
       <v-list-item prepend-icon="mdi-help-box" title="Help" value="help"></v-list-item>
       <v-list-item to="status" prepend-icon="mdi-list-status" title="Server Status" value="serverStatus"></v-list-item>
       <v-list-item prepend-icon="mdi-book-information-variant" title="Quick Reference" value="wiki"></v-list-item>
-      <v-list-item prepend-icon="mdi-bug" title="Report a bug" value="starred"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item prepend-icon="mdi-bug" title="Report a bug" value="bug" :active="false"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -57,12 +59,14 @@
 <script setup>
 import logo from "@/assets/logo.svg";
 import { useRoute } from "vue-router";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const logoSrc = ref();
 const openGroups = ref([]);
 const route = useRoute();
-
+const activeModules = computed(() => {
+  return route.path === '/network' ? true : false
+})
 function onDrawerCollapse(value) {
   if (value) {
     openGroups.value = [];
---
+++ askcos_vue/src/views/network/tabs/RetroView.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container fluid>
+        <v-row class="justify-center align-center">
+            <v-col cols="12" md="3"><v-sheet elevation="2">
+                    <div id="retro-input" class="d-flex justify-content-center">
+                        <b-input-group>
+                            <b-input-group-prepend>
+                                <b-input-group-text>Target</b-input-group-text>
+                                <b-input-group-text v-if="enableResolver" @click="toggleResolver" role="button">
+                                    <i v-if="allowResolve" v-b-tooltip style="color: darkgreen" class="fas fa-server"
+                                        title="Connection to NIH name resolver is ON, structures may be sent to an external service. This can be turned off in the settings menu, or by clicking this icon."></i>
+                                    <i v-else v-b-tooltip style="color: darkred" class="fas fa-server"
+                                        title="Connection to NIH name resolver is OFF, structures will NOT be sent to an external service. Target query must be a SMILES string. This can be turned on in the settings menu, or by clicking this icon."></i>
+                                </b-input-group-text>
+                            </b-input-group-prepend>
+                            <b-input id="retro-target" v-model="target" @blur="resolve" @keyup.enter="resolve"></b-input>
+                            <b-input-group-append>
+                                <b-button v-b-modal.retro-ketcher-modal>
+                                    <i class="fas fa-edit"></i>
+                                </b-button>
+                            </b-input-group-append>
+                        </b-input-group>
+                    </div>
+                </v-sheet></v-col>
+            <v-col cols="12" md="9"><v-sheet elevation="2">H</v-sheet></v-col>
+        </v-row>
+    </v-container>
+</template>
