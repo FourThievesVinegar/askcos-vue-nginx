065aa0d6c5cb032c687285437d980ff06b91c929
Samuel Gong <gosamuel719@gmail.com>
2023-09-21 09:56:12 -0400

    banlist script setup

--- askcos_vue/src/views/banlist/Banlist.vue
+++ askcos_vue/src/views/banlist/Banlist.vue
@@ -53,11 +53,169 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import banlist from "@/assets/banlist.svg";
+import { ref, computed, onMounted } from 'vue';
+import SmilesImage from "@/components/SmilesImage";
+import { API } from "@/common/api";
+import dayjs from 'dayjs';
+
 
 const buyables = ref([]);
 const tab = ref("")
+const activeTab = ref(0);
+const chemicals = ref([]);
+const reactions = ref([]);
+const newSmiles = ref('');
+const newDesc = ref('');
+const newActive = ref(true);
+const newType = ref('chemicals');
+const filterActive = ref('all');
+const pendingTasks = ref(0);
+const filterOptions = ref([
+  { text: 'All', value: 'all' },
+  { text: 'Active', value: 'active' },
+  { text: 'Inactive', value: 'inactive' },
+]);
+
+const loadCollection = (category) => {
+  pendingTasks.value++;
+  API.get(`/api/banlist/${category}/get`)
+    .then(json => {
+      json.forEach(function (doc) {
+        doc.created = dayjs(doc.created).format('MMMM D, YYYY h:mm A');
+      });
+      if (category === 'chemicals') {
+        chemicals.value = json
+        console.log(chemicals.value)
+      } else {
+        reactions.value = json
+      }
+    })
+    .finally(() => {
+      pendingTasks.value--;
+    })
+}
+
+
+
+onMounted(() => {
+  loadCollection('chemicals');
+  loadCollection('reactions');
+});
+
+const filteredChemicals = computed(() => {
+  if (filterActive.value === 'active') {
+    return chemicals.value.filter(entry => entry.active)
+  } else if (filterActive.value === 'inactive') {
+    return chemicals.value.filter(entry => !entry.active)
+  } else {
+    return chemicals.value
+  }
+});
+
+const filteredReactions = computed(() => {
+  if (filterActive.value === 'active') {
+    return reactions.value.filter(entry => entry.active)
+  } else if (filterActive.value === 'inactive') {
+    return reactions.value.filter(entry => !entry.active)
+  } else {
+    return reactions.value
+  }
+});
+
+const showLoader = computed(() => {
+  return pendingTasks.value > 0
+});
+
+const addEntry = () => {
+  pendingTasks.value++;
+  const body = {
+    smiles: newSmiles.value,
+    description: newDesc.value || 'no description',
+    active: newActive.value,
+  };
+  API.post(`/api/v2/banlist/${newType.value}/`, body)
+    .then(json => {
+      json.created = dayjs(json.created).format('MMMM D, YYYY h:mm A');
+      if (newType.value === 'chemicals') {
+        chemicals.value.push(json);
+        activeTab.value = 0
+      } else {
+        reactions.value.push(json);
+        activeTab.value = 1
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      pendingTasks.value--;
+    })
+}
+
+const deleteEntry = (id, category) => {
+  pendingTasks.value++;
+  if (window.confirm('Click "OK" to confirm deleting this entry')) {
+    API.delete(`/api/v2/banlist/${category}/${encodeURIComponent(id)}/`)
+      .then(json => {
+        if (json['success']) {
+          const collection = category === 'chemicals' ? chemicals.value : reactions.value
+          for (let i = 0; i < collection.length; i++) {
+            if (collection[i]['id'] === id) {
+              collection.splice(i, 1)
+              break
+            }
+          }
+        }
+      })
+      .finally(() => {
+        pendingTasks.value--;
+      })
+  }
+}
+
+const deleteChemical = (id) => {
+  deleteEntry(id, 'chemicals')
+}
+
+const deleteReaction = (id) => {
+  deleteEntry(id, 'reactions')
+}
+
+const toggleActivation = (id, category, action) => {
+  pendingTasks.value++;
+  API.get(`/api/v2/banlist/${category}/${encodeURIComponent(id)}/${action}/`)
+    .then(json => {
+      if (json['success']) {
+        const updatedEntry = json['data'];
+        updatedEntry.created = dayjs(updatedEntry.created).format('MMMM D, YYYY h:mm A');
+        const collection = category === 'chemicals' ? chemicals.value : reactions.value
+        for (let i = 0; i < collection.length; i++) {
+          if (collection[i]['id'] === id) {
+            collection.splice(i, 1, updatedEntry)
+            break
+          }
+        }
+      }
+    })
+    .finally(() => {
+      pendingTasks.value--;
+    })
+}
+
+const activateChemical = (id) => {
+  toggleActivation(id, 'chemicals', 'activate')
+}
+
+const deactivateChemical = (id) => {
+  toggleActivation(id, 'chemicals', 'deactivate')
+}
+
+const activateReaction = (id) => {
+  toggleActivation(id, 'reactions', 'activate')
+}
+
+const deactivateReaction = (id) => {
+  toggleActivation(id, 'reactions', 'deactivate')
+}
 
 </script>
 
