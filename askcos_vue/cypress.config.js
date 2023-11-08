b13be962eb16f25b3c9f853f473a439e96e8c473
Fong Mun Hong <fongmh1947@gmail.com>
2023-11-08 17:53:05 -0500

    e2e tests

--- askcos_vue/cypress.config.js
+++ askcos_vue/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://3.16.7.29/',
     video: false,
   }
 },
