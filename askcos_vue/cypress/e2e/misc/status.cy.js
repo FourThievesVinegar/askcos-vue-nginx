70657e9e1862d84e9b5bff28d8f24dd48f2e3b8b
Samuel Gong <gosamuel719@gmail.com>
2023-11-09 14:07:15 -0500

    some cypress changes

--- askcos_vue/cypress/e2e/misc/status.cy.js
+++ askcos_vue/cypress/e2e/misc/status.cy.js
@@ -1,27 +1,27 @@
 /* global cy */
 describe('Status Page', () => {
-    beforeEach(() => {
-      cy.visit('/status')
-    })
-
-    it('Should display the title correctly', () => {
-      cy.get('h3.text-h5').should('contain', 'Celery Worker Status', 'Database Status')
-    })
-
-    it('Should display refresh button', () => {
-      cy.get('[data-cy="refresh-button"]').should('be.visible')
-    })
-
-    it('Should load and display data table after fetching', () => {
-      cy.get('.v-data-table').should('be.visible')
-      cy.get('.v-data-table').find('tbody').find('tr').its('length').should('be.gt', 0)
-    })
-
-    it('Clicking refresh should trigger data refresh', () => {
-      cy.intercept('GET', '/api/v2/status/celery/').as('getStatus')
-      cy.get('[data-cy="refresh-button"]').click()
-      cy.wait('@getStatus') 
-      cy.get('span.text-body-2').should('contain', 'Last Update:')
-    })
-
+  beforeEach(() => {
+    cy.visit('/status')
   })
+
+  it('Should display the title correctly', () => {
+    cy.get('h3.text-h5').should('contain', 'Celery Worker Status', 'Database Status')
+  })
+
+  it('Should display refresh button', () => {
+    cy.get('[data-cy="refresh-button"]').should('be.visible')
+  })
+
+  it('Should load and display data table after fetching', () => {
+    cy.get('.v-data-table').should('be.visible')
+    cy.get('.v-data-table').find('tbody').find('tr').its('length').should('be.gt', 0)
+  })
+
+  it('Clicking refresh should trigger data refresh', () => {
+    cy.intercept('GET', '/api/status/celery/get').as('getStatus')
+    cy.get('[data-cy="refresh-button"]').click()
+    cy.wait('@getStatus')
+    cy.get('span.text-body-2').should('contain', 'Last Update:')
+  })
+
+})
