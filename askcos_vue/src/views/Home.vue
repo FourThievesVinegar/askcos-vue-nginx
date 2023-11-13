56933a0adec0e86c56ae24766b4ef7acb3cb75ef
Sourabh Choure <sourabhchoure98@gmail.com>
2023-11-13 05:58:18 +0000

    Fix home page

--- askcos_vue/src/views/Home.vue
+++ askcos_vue/src/views/Home.vue
@@ -1,12 +1,9 @@
 <template>
-  <v-container fluid style="min-height: calc(100vh-50px)">
-    <v-row class="justify-center">
-      <v-col cols="12" md="12" xl="10">
-        <!-- <h1 class="header my-6 text-center">
-          ASKCOS: Software tools for organic synthesis
-        </h1> -->
+  <v-container class="fill-height" fluid style="min-height: calc(100vh-50px);">
+    <v-row>
+      <v-col cols="12" md="12" xl="10" class="d-flex justify-center flex-column">
         <v-expand-transition>
-          <v-sheet elevation="2" rounded="lg" width="100%" class="my-6 pa-6" v-show="show">
+          <v-sheet elevation="2" rounded="lg" width="100%" class="mb-6 pa-6" v-show="show">
             <v-row class="justify-center align-center pa-0 ma-0">
               <v-col cols="12" class="d-flex justify-center align-center">
                 <h2 class="text-h2 text-blue-darken-3 header font-weight-medium"><strong>Explore ASKCOS</strong></h2>
