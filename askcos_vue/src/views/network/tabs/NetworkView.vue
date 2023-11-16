4f1896050c36fb3c3051eee54d2934a3c550f3d0
Sourabh Choure <sourabhchoure98@gmail.com>
2023-11-16 23:31:23 +0000

    Check for empty file

--- askcos_vue/src/views/network/tabs/NetworkView.vue
+++ askcos_vue/src/views/network/tabs/NetworkView.vue
@@ -258,7 +258,7 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="primary" @click="showImportNetwork = false">Cancel</v-btn>
-        <v-btn color="primary" @click="() => { showImportNetwork = false; load() }">Load</v-btn>
+        <v-btn color="primary" :disabled="!uploadFile" @click="() => { showImportNetwork = false; load() }">Load</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
