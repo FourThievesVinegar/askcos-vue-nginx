83c7ba004e9ea7b0134ce90340ff18aa4a3d5929
Sourabh Choure <sourabhchoure98@gmail.com>
2023-11-28 19:55:14 +0000

    Change Cypress Config

--- askcos_vue/cypress.config.js
+++ askcos_vue/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:3000',
     // baseUrl: 'http://3.16.7.29/',
     video: false,
   }
