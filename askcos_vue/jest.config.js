88a564a7a2ac145b86afc33cfbe05c9e1e8baa0c
Sourabh Choure <sourabhchoure98@gmail.com>
2023-11-29 21:18:01 +0000

    Change test coverage files

--- askcos_vue/jest.config.js
+++ askcos_vue/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
+  collectCoverageFrom: ["src/common/*.js", "!**/node_modules/**"],
   coverageReporters: ["html", "text", "text-summary", "cobertura"],
   testMatch: ["**/*.test.js"],
 };
--- askcos_vue/src/common/resolver.js
+++ askcos_vue/src/common/resolver.js
@@ -2,15 +2,13 @@
  * External chemical name resolver utility
  */
 
-function resolveChemName(name) {
+async function resolveChemName(name) {
   const url = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${encodeURIComponent(name)}/property/IsomericSMILES/txt`;
-  return fetch(url)
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      }
-      throw new Error(`Unable to resolve "${name}" to SMILES.`);
-    });
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.text();
+  }
+  throw new Error(`Unable to resolve "${name}" to SMILES.`);
 }
 
 export { resolveChemName };
