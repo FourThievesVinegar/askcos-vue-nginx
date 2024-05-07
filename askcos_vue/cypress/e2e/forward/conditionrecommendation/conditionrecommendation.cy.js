e84601768e708a9fa75117e2f22973ae38045679
Sourabh Choure <sourabhchoure98@gmail.com>
2024-05-07 08:20:17 -0400

    add condition rec test

---
+++ askcos_vue/cypress/e2e/forward/conditionrecommendation/conditionrecommendation.cy.js
@@ -0,0 +1,36 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+describe("Condition Recommendation Page", () => {
+  beforeEach(() => {
+    cy.viewport("macbook-11");
+    cy.visit("/forward?tab=context");
+  });
+
+  it("can input Reactants", function () {
+    cy.get('[data-cy="reactants"]').type("BrBr.c1ccccc1");
+  });
+
+  it("can input Product", function () {
+    cy.get('[data-cy="reactants"]').type("BrBr.c1ccccc1");
+    cy.get('[data-cy="product"]').type("Brc1ccccc1");
+  });
+
+  it("Click to open the setting menu", function () {
+    cy.get("button").contains("Settings").click();
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(".v-card-title").should("be.visible");
+    /* ==== End Cypress Studio ==== */
+  });
+
+  it("Test SMILES", function () {
+    cy.get('[data-cy="reactants"]').type("ClC1CCCCC1.OC1CCCCC1");
+    cy.get('[data-cy="product"]').type("C1CCC(OC2CCCCC2)CC1");
+    cy.get('[data-cy="submit-button"] > .v-btn__content').click();
+    cy.get("tbody").children().should("have.length", 10);
+    // cy.get('tbody').children().each(($el, index) => {
+    //     // index starts at 0, so we add 1 to get the correct child number
+    //     cy.get(`tbody > :nth-child(${index + 1}) > :nth-child(5)`).should('contain', '°C');
+    // });
+  });
+});
