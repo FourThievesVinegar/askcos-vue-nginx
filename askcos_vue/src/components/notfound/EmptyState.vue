7c3884eb35b514f77300e957697d10f10323ded5
Sourabh Choure <sourabhchoure98@gmail.com>
2024-05-15 17:59:23 -0400

    Seperate notfound into it's own component

---
+++ askcos_vue/src/components/notfound/EmptyState.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row class="justify-center">
+        <v-col cols="12" md="10" class="d-flex justify-center text-center">
+            <v-empty-state title="Page not found"
+                text="This page didn't bond with any of our known pages" :image="pageNotFoundSrc">
+                <template v-slot:actions>
+                    <v-btn variant="tonal" to="/">Take me home</v-btn>
+                </template>
+            </v-empty-state>
+        </v-col>
+    </v-row>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import pageNotFound from "@/assets/pageNotFound.svg";
+
+const pageNotFoundSrc = ref(pageNotFound)
+</script>
